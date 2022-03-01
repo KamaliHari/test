5b00dee8bbfbafd877cd7856b7b3cb820e88eadb
KamaliHari <kamalihari2002@gmail.com>
2022-03-01 18:21:39 +0530

    init

---
+++ test.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+public class test {
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3307/jdbcdemo";
+        String username="root";
+        String password="";
+    
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection= DriverManager.getConnection(url,username,password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from student");
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(1)+" "+resultSet.getString(2));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+      
+    } 
+}
